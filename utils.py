a0b7fee26761b0c70aeaf78450dfbfef9eaa1a95
llang6 <45853557+llang6@users.noreply.github.com>
2025-04-14 16:48:54 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,12 +5,12 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 
 def load_data():
-    path = 'src/customDatabase.xlsx'
+    path = 'customDatabase.xlsx'
     df = pd.read_excel(path)
-    with open('src/chemvecs_mol2vec.pkl', 'rb') as file:
+    with open('chemvecs_mol2vec.pkl', 'rb') as file:
         chemvecs_mol2vec = pickle.load(file)
         file.close()
-    with open('src/chemvecs_morgan.pkl', 'rb') as file:
+    with open('chemvecs_morgan.pkl', 'rb') as file:
         chemvecs_morgan = pickle.load(file)
         file.close()
     return df, chemvecs_mol2vec, chemvecs_morgan
